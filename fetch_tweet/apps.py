9e33a2d973f0016dcec653111c58c06bcbc79f56
Kanishk Dugar <kanishk.dugar@outlook.com>
2017-10-22 17:08:48 -0400

    remove keys

--- fetch_tweet/apps.py
+++ fetch_tweet/apps.py
@@ -11,17 +11,17 @@ class FetchTweetConfig(AppConfig):
     name = 'fetch_tweet'
 
     # def ready(self):
-    #     ACCESS_TOKEN = '372468931-o72HBeeIG0VaTdTmebbkkLIvLNbDvca67PqjWO7H'
-    #     ACCESS_SECRET = 'NKuBvWIWuPTaZhSeu6jKUVMPRvmAFee36kv7XDtOnh4IB'
-    #     CONSUMER_KEY = 'TCPKGo6IsyKJjEKPpxvF115V0'
-    #     CONSUMER_SECRET = 'hvUjIHG3bjkSssXOAIkxhGILdyKZu0AlDfNy0QhVZBvLcJNOgE'
+    #     ACCESS_TOKEN = ''
+    #     ACCESS_SECRET = ''
+    #     CONSUMER_KEY = ''
+    #     CONSUMER_SECRET = ''
     #     oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
     #
     #     twitter_stream = TwitterStream(auth=oauth)
     #
     #     iterator = twitter_stream.statuses.filter(track="pollution,LGBTQ,vegan,North Korea,Modi")
     #
-    #     awsauth = AWS4Auth('AKIAIEXN5M445JGBK5CA', 'bbY++JHJOhaqYZiG5Wa+mUm+OnqV8keUs/vXju4d', 'us-east-1', 'es')
+    #     awsauth = AWS4Auth('', '', 'us-east-1', 'es')
     #
     #     host = 'search-tweet-store-gsxdukg3yfbejw5oabidx4wf5y.us-east-1.es.amazonaws.com'
     #
