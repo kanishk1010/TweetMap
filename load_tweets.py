cce2ab6f241e735a871de5ce5434bf2dd0a87cfb
kanishk1010 <kanishk.dugar@outlook.com>
2017-10-28 14:20:23 -0400

    updated load-tweets.py

--- load_tweets.py
+++ load_tweets.py
@@ -10,15 +10,10 @@ CONSUMER_KEY = ''
 CONSUMER_SECRET = ''
 
 oauth = OAuth(ACCESS_TOKEN, ACCESS_SECRET, CONSUMER_KEY, CONSUMER_SECRET)
-
 twitter_stream = TwitterStream(auth=oauth)
-
 iterator = twitter_stream.statuses.filter(track="pollution,LGBTQ,vegan,North Korea, Modi")
-
 awsauth = AWS4Auth('', '', 'us-east-1', 'es')
-
 host = 'search-tweet-store-gsxdukg3yfbejw5oabidx4wf5y.us-east-1.es.amazonaws.com'
-
 es = Elasticsearch(
     hosts=[{'host': host, 'port': 443}],
     http_auth=awsauth,
@@ -28,16 +23,28 @@ es = Elasticsearch(
     timeout=30
 )
 
-# es.indices.delete(index="tweets")
-if es.indices.exists(index="tweets"):
-        print("index tweets exists")
+index_exists = None
+while index_exists is None:
+    try:
+        index_exists = es.indices.exists(index="tweets")
+    except Exception:
+        time.sleep(30)
+
+if index_exists:
+    print("index tweets exists")
 else:
     request_body = {
         "settings": {
             "index.mapping.total_fields.limit": 2000
         }
     }
-    es.indices.create(index="tweets", body=request_body)
+    index_response = None
+
+    while index_response is None:
+        try:
+            index_response = es.indices.create(index="test", body=request_body)
+        except Exception:
+            time.sleep(30)
 
 for tweet in iterator:
     document = json.dumps(tweet)
