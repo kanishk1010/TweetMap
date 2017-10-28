4a154532948d4da541885485ebdbd7f7c84f9359
kanishk1010 <kanishk.dugar@outlook.com>
2017-10-28 14:07:25 -0400

    client side geocoding

--- fetch_tweet/views.py
+++ fetch_tweet/views.py
@@ -4,8 +4,8 @@ from elasticsearch import Elasticsearch, RequestsHttpConnection
 from requests_aws4auth import AWS4Auth
 import googlemaps
 
-gmaps = googlemaps.Client(key='')
-awsauth = AWS4Auth('', '', 'us-east-1', 'es')
+gmaps = googlemaps.Client(key='AIzaSyAlZCLQckOd17qOFDVyFJk9TN8mIvH4zMs')
+awsauth = AWS4Auth('AKIAIW4WGIXUESC3K4SQ', 'kiX8y+gNAHeduKXd8tURF1H3w1hRQ99bUIFw4axy', 'us-east-1', 'es')
 host = 'search-tweet-store-gsxdukg3yfbejw5oabidx4wf5y.us-east-1.es.amazonaws.com'
 es = Elasticsearch(
     hosts=[{'host': host, 'port': 443}],
@@ -22,31 +22,33 @@ def generate_plot(keyword):
     res = es.search(index="tweets", doc_type="tweet", size=100, body={"query": {"match": {"text": keyword}}})
     print("Got %d Hits:" % res['hits']['total'])
     coordinates = []
+    locations = []
     for hit in res['hits']['hits']:
         if hit["_source"]["coordinates"] is None:
             if hit["_source"]["user"]["location"]:
-                try:
-                    geocode_result = gmaps.geocode(hit["_source"]["user"]["location"])
-                    lat = geocode_result[0]['geometry']['location']['lat']
-                    long = geocode_result[0]['geometry']['location']['lng']
-                except Exception:
-                    lat, long = uniform(-90,90), uniform(-180,180)
+                # try:
+                locations.append(hit["_source"]["user"]["location"])
+                # geocode_result = gmaps.geocode(hit["_source"]["user"]["location"])
+                # lat = geocode_result[0]['geometry']['location']['lat']
+                # long = geocode_result[0]['geometry']['location']['lng']
+                # except Exception:
+                #     lat, long = uniform(-90,90), uniform(-180,180)
             else:
-                lat, long = uniform(-90,90), uniform(-180,180)
+                lat, long = uniform(-90, 90), uniform(-180, 180)
+                coordinates.append({"lat": lat, "lng": long})
         else:
             long, lat = hit["_source"]["coordinates"]["coordinates"]
-        coordinates.append({"lat": lat, "lng": long})
-        print(lat,long)
-    return coordinates
+            coordinates.append({"lat": lat, "lng": long})
+            # print(lat,long)
+    return {'coordinates': coordinates, 'locations': locations}
 
 
 def index(request):
-    return render(request, 'fetch_tweet/index.html', {'coordinates': []})
+    return render(request, 'fetch_tweet/index.html', {'coordinates': [], 'locations': []})
 
 
 def search(request, keyword):
     context = generate_plot(keyword)
-    print(len(context))
-    return render(request, 'fetch_tweet/index.html', {'coordinates': context})
-
-
+    print(len(context['coordinates']))
+    print(len(context['locations']))
+    return render(request, 'fetch_tweet/index.html', context)
