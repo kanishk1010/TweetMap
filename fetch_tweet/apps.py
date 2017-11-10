a52b85b7437201fd79634ee5b1136fe3c63d7e99
Kanishk Dugar <kanishk.dugar@outlook.com>
2017-11-09 23:36:38 -0500

    Update apps.py

--- fetch_tweet/apps.py
+++ fetch_tweet/apps.py
@@ -1,11 +1,4 @@
 from django.apps import AppConfig
-import gmplot
-from elasticsearch import Elasticsearch, RequestsHttpConnection
-from requests_aws4auth import AWS4Auth
-from random import uniform
-import json
-from twitter import OAuth, TwitterStream
-
 
 class FetchTweetConfig(AppConfig):
     name = 'fetch_tweet'
